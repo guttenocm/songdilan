09d50886a2a69daa2f5d4780146a42bce57244b3
guttenocm <85875188+guttenocm@users.noreply.github.com>
2021-06-14 06:54:12 -0400

    Update get.sh

--- get.sh
+++ get.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
-
-while :
+while true
 do
-  if ping -s 1000 -c 1 amazonaws.com &> /dev/null
-  then
-    echo "Host is online"
-  fi
-  sleep 90
+        date1=$(date +%s%N)
+        php /var/www/html/sprint/yii hello/validate
+        wait
+        diff=$(echo "($(date +%s%N) - $date1)/1000000" | bc -l)
+        echo "That took $diff milliseconds."
+        time sleep $(echo "1.2 - ($diff)/1000" | bc -l)
 done
